e95df92895e7d20140206ce018280100af333f3c
aslihancelik <aslihancelik27@gmail.com>
2024-10-04 21:09:17 -0400

    adding functions

--- CSC222_FunctionsProject.cpp
+++ CSC222_FunctionsProject.cpp
@@ -12,7 +12,13 @@ void calcDistance(double, double);
 // Precondition: It takes as input the coordinates for the center of a circle 
 //              and the coordinates of a point on the circumference
 // Postcondition: It returns the radius of the circle
+double calcRadius(double, double, double, double);
 
+double calcDistance(double, double, double, double);
+
+double calcCircumference(double);
+
+double calcArea(double);
 
 int main()
 {
@@ -20,5 +26,18 @@ int main()
 }
 
 void calcDistance(double x, double y) {
+
+}
+
+double calcRadius(double xCenter, double yCenter, double xCircum, double yCircum) {
+
+}
+double calcDistance(double x1, double y1, double x2, double y2) {
+
+}
+double calcCircumference(double radius) {
+
+}
+double calcArea(double radius) {
 
 }
